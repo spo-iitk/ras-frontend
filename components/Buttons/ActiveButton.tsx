6652d58003d1ba5b1c53ff8a7eeb395ec17c4e6a
Manas Gupta <mmanasggupta@gmail.com>
2022-05-22 16:43:38 +0530

    Create applications page and styles

---
+++ components/Buttons/ActiveButton.tsx
@@ -0,0 +1,16 @@
+import { red, green } from '@mui/material/colors';
+import Button, { ButtonProps } from '@mui/material/Button';
+import styled from '@emotion/styled';
+
+const ActiveButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    color: 'white',
+    borderRadius: 20,
+    padding: '0.5rem 1rem',
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[500],
+    },
+    width: 100,
+  }));
+
+export default ActiveButton;
