441eb67ce1997068497634d2083f8197492f18ac
Aadarsh <aadarshshaw05@gmail.com>
2022-06-01 23:26:02 +0530

    Student-Enrollment & QnA Pages

--- pages/student/rc/[rcid]/enrollment.tsx
+++ pages/student/rc/[rcid]/enrollment.tsx
@@ -5,10 +5,47 @@ import {
   Typography,
   Grid,
   Button,
+  Box,
 } from "@mui/material";
 import Meta from "../../../../components/Meta";
 
 const Enrollment = () => {
+  const data = 
+  {
+    "title": "Enrollment Secition",
+    "text": [
+      {
+        "id": 15659,
+        "question": "What is the maximum number of passengers that can be carried by a single aircraft?",
+        "isMandatory": true
+      },
+      {
+        "id": 15661,
+        "question": "What is the maximum number of passengers that can be thrown from a single aircraft?",
+        "isMandatory": false
+      }
+    ],
+    "mcq": [
+      {
+        "id": 156559,
+        "question": "What is the best way to get ailerons and flaps to the right position?",
+        "Options": "Ailerons to the right, flaps to the maximum, flaps to the minimum, Ailerons to the left",
+        "isMandatory": true
+      },
+      {
+        "id": 156561,
+        "question": "What is your fav option?",
+        "Options": "A,B,C,D,E,F",
+        "isMandatory": false
+      },
+      {
+        "id": 156562,
+        "question": "What is the best way to get ailerons and flaps to the minimum position?",
+        "Options": "Ailerons to the left, flaps to the minimum, flaps to the maximum, Ailerons to the right",
+        "isMandatory": false
+      }
+    ]
+  }
   const styles = {
     "& .MuiInputLabel-root": { color: "black" },
     "& .MuiOutlinedInput-root": {
@@ -18,8 +55,8 @@ const Enrollment = () => {
       "& > fieldset": { borderColor: "black" },
     },
   };
-  const [toggleStates, setToggleStates] = React.useState(() => [false, false, false]);
-
+  const [toggleStates, setToggleStates] = React.useState(() => [...Array(data.mcq.length).fill(false)]);
+  const [i,setNext] = React.useState(0)
   const handleChange = (
     index: number
   ) => {
@@ -39,67 +76,56 @@ const Enrollment = () => {
           mx: "auto",
           mt: "2%",
           minHeight:"70vh",
-          maxHeight: "90vh",
-          width: "60vh",
+          width: "80vh",
           justifyContent: "space-around",
           padding: "1%",
-          border: 1,
           borderRadius: 2,
           boxShadow: 4,
           fontWeight: "light",
         }}
       >
-        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-          Enrollment Section
+        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+          {data.title}
         </Typography>
-        <Typography variant="subtitle1">Question 1</Typography>
-        <TextField
-          sx={styles}
-          id="outlined-basic"
-          label="Enter your answer"
-          variant="outlined"
-          fullWidth
-        />
-        <Typography variant="subtitle1">Question 2</Typography>
-        <TextField
-          sx={styles}
-          id="outlined-basic"
-          label="Enter your answer"
-          variant="outlined"
-          fullWidth
-        />
-        <Typography variant="subtitle1">Question 3</Typography>
-        <ToggleButton
-          sx={{justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected':{backgroundColor:"#bdbdbd"}}}
-          value="opt1"
-          selected={toggleStates[0]}
-          onClick={()=>handleChange(0)}
-          aria-label="left aligned"
-        >
-          Option 1
-        </ToggleButton>
-        <ToggleButton
-          sx={{ justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected':{backgroundColor:"#bdbdbd"}}}          value="opt2"
-          selected={toggleStates[1]}
-          onClick={()=>handleChange(1)}
-          aria-label="left aligned"
-        >
-          Option 2
-        </ToggleButton>
-        <ToggleButton
-          sx={{justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected':{backgroundColor:"#bdbdbd"}}}
-          value="opt3"
-          selected={toggleStates[2]}
-          onClick={()=>handleChange(2)}
-          aria-label="left aligned"
-        >
-          Option 3
-        </ToggleButton>
-
+        <Typography variant="subtitle2" sx={{ fontWeight: "light" }}>
+          *Required
+        </Typography>
+        {data.text.map((d)=>(
+          <Box sx={{padding:"5px"}}>
+          <Typography variant="subtitle1">{d.question}</Typography>
+          <TextField
+            sx={styles}
+            id="outlined-basic"
+            label="Enter your answer"
+            variant="outlined"
+            fullWidth
+            required={d.isMandatory}
+          />
+          </Box>
+        ))}
+        {data.mcq.map((d)=>(
+          <Box sx={{padding:"5px"}}>
+          <Typography variant="subtitle1">{d.question}</Typography>
+          {d.Options.split(',').map((opt) =>(
+            <ToggleButton
+            sx={{justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected, &.Mui-selected:hover':{backgroundColor:"#6CB4EE"}}}
+            value={opt}
+            selected={toggleStates[i]}
+            onClick={()=>handleChange(i)}
+            aria-label="left aligned"
+            fullWidth
+          >
+            {opt}
+          </ToggleButton>
+          ))}
+          </Box>
+        ))}
         <Button
           variant="contained"
           fullWidth
           sx={{
+            width:"100%",
+            padding:"16.5px 14px",
             backgroundColor: "#170e0d",
             "&:hover": {
               backgroundColor: "black",
