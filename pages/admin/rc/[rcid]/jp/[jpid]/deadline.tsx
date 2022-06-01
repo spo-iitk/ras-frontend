a60e628b69d86809433882e426b6d6de337ae594
Manas Gupta <mmanasggupta@gmail.com>
2022-06-01 15:33:41 +0530

    Add more routes to admin pages

---
+++ pages/admin/rc/[rcid]/jp/[jpid]/deadline.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Deadline() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Deadline
---
+++ pages/admin/rc/[rcid]/jp/[jpid]/sendMail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SendMail() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SendMail
