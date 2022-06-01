653efc66fc02e925c22ae89d57fb41ee5d2b5ece
Aadarsh <aadarshshaw05@gmail.com>
2022-06-01 15:05:27 +0530

    Added Pages enrollment and custom q/a

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 const index = () => {
   return (
     <div>
-      
     </div>
   )
 }
---
+++ pages/studentDashboard/rc/[rcid]/enrollment.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import {
+  TextField,
+  ToggleButton,
+  Typography,
+  Grid,
+  Button,
+} from "@mui/material";
+import Meta from "../../../../components/Meta";
+
+const Enrollment = () => {
+  const styles = {
+    "& .MuiInputLabel-root": { color: "black" },
+    "& .MuiOutlinedInput-root": {
+      "& > fieldset": { borderColor: "black" },
+    },
+    "& .MuiOutlinedInput-root.Mui-focused": {
+      "& > fieldset": { borderColor: "black" },
+    },
+  };
+  const [toggleStates, setToggleStates] = React.useState(() => [false, false, false]);
+
+  const handleChange = (
+    index: number
+  ) => {
+    const newStates = [...toggleStates]
+    newStates[index]  =!newStates[index]
+    setToggleStates(newStates);
+  };
+
+  return (
+    <div>
+      <Meta title="Student Dashboard - Enrollment Section" />
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        sx={{
+          mx: "auto",
+          mt: "2%",
+          minHeight:"70vh",
+          maxHeight: "90vh",
+          width: "60vh",
+          justifyContent: "space-around",
+          padding: "1%",
+          border: 1,
+          borderRadius: 2,
+          boxShadow: 4,
+          fontWeight: "light",
+        }}
+      >
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          Enrollment Section
+        </Typography>
+        <Typography variant="subtitle1">Question 1</Typography>
+        <TextField
+          sx={styles}
+          id="outlined-basic"
+          label="Enter your answer"
+          variant="outlined"
+          fullWidth
+        />
+        <Typography variant="subtitle1">Question 2</Typography>
+        <TextField
+          sx={styles}
+          id="outlined-basic"
+          label="Enter your answer"
+          variant="outlined"
+          fullWidth
+        />
+        <Typography variant="subtitle1">Question 3</Typography>
+        <ToggleButton
+          sx={{justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected':{backgroundColor:"#bdbdbd"}}}
+          value="opt1"
+          selected={toggleStates[0]}
+          onClick={()=>handleChange(0)}
+          aria-label="left aligned"
+        >
+          Option 1
+        </ToggleButton>
+        <ToggleButton
+          sx={{ justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected':{backgroundColor:"#bdbdbd"}}}          value="opt2"
+          selected={toggleStates[1]}
+          onClick={()=>handleChange(1)}
+          aria-label="left aligned"
+        >
+          Option 2
+        </ToggleButton>
+        <ToggleButton
+          sx={{justifyContent:"flex-start", border: "1.5px solid black" ,'&.Mui-selected':{backgroundColor:"#bdbdbd"}}}
+          value="opt3"
+          selected={toggleStates[2]}
+          onClick={()=>handleChange(2)}
+          aria-label="left aligned"
+        >
+          Option 3
+        </ToggleButton>
+
+        <Button
+          variant="contained"
+          fullWidth
+          sx={{
+            backgroundColor: "#170e0d",
+            "&:hover": {
+              backgroundColor: "black",
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </Grid>
+    </div>
+  );
+};
+Enrollment.layout = "studentDashboard";
+export default Enrollment;
