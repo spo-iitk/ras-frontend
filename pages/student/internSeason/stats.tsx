19c94afd385ade5220ee7d3bc0712d3b8760a52f
Manas Gupta <mmanasggupta@gmail.com>
2022-06-02 17:24:18 +0530

    Fixed responsiveness of Stats page

--- pages/student/internSeason/stats.tsx
+++ pages/student/internSeason/stats.tsx
@@ -2,49 +2,42 @@ import React from 'react'
 import Meta from '../../../components/Meta'
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Stack, Grid } from '@mui/material';
-
+import styles from "../../../styles/studentInternPhase.module.css";
 const columns: GridColDef[] = [
+  {
+    field: 'id',
+    headerName: 'Id',
+    width: 100
+  },
   {
     field: 'Name',
     headerName: 'Name',
-    align: 'center',
-    headerAlign: 'center',
     width: 200
   },
   {
     field: 'Roll_no',
     headerName: 'Roll No.',
-    align: 'center',
-    headerAlign: 'center',
     width: 200
   },
   {
     field: 'Company_Name',
     headerName: 'Company Name',
-    align: 'center',
-    headerAlign: 'center',
     width: 200
   },
   {
     field: 'Designation',
     headerName: 'Designation',
-    align: 'center',
-    headerAlign: 'center',
     width: 200
   },
   {
     field: 'Program',
     headerName: 'Program',
-    align: 'center',
-    headerAlign: 'center',
     width: 200
   },
   {
     field: 'Branch',
     headerName: 'Branch',
-    align: 'center',
-    headerAlign: 'center',
-    width: 200
+    width: 100
   }
 ];
 const rows = [
@@ -53,19 +46,16 @@ const rows = [
 
 function Stats() {
   return (
-    <div>
+    <div className={styles.container}>
         <Meta title="Statistics - Intern Season" />
         <Stack>
-          <div style={{ height: 500, margin: '0px 2rem'}} >
-            <Grid container spacing={1}>
-              <Grid item xs={12} style={gridMain}>
-                <h1>Stats &gt; Student Wise</h1>
-              </Grid>
-              </Grid>
+          <h1>Stats</h1>
+          <div style={{ height: 500, margin: '0px auto'}} className={styles.datagridStats}>
               <DataGrid
               rows={rows}
               columns={columns}
               pageSize={7}
+              rowsPerPageOptions={[7]}
             />
           </div>
         </Stack>
@@ -76,11 +66,3 @@ function Stats() {
 
 Stats.layout="studentPhaseDashboard"
 export default Stats
-
-const gridMain = {
-  width: "100%",
-  padding: "0",
-  margin: "0",
-  alignContent: "right",
-  alignItems:"right",
-};
