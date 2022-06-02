7baf5b202792208152db61628da3f9470672cf2d
Manas Gupta <mmanasggupta@gmail.com>
2022-06-02 22:18:32 +0530

    Fix responsiveness applications.tsx

--- pages/student/rc/[rcId]/applications.tsx
+++ pages/student/rc/[rcId]/applications.tsx
@@ -1,41 +1,39 @@
 import React from 'react'
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Grid,Stack } from '@mui/material'
-import styles from '/styles/internPhase.module.css'
+import styles from '@styles/internPhase.module.css'
 import Meta from '@components/Meta';
 const columns: GridColDef[] = [
   {
     field: 'id',
+    headerName: 'Id',
+    width: 100
+  },
+  {
+    field: 'companyName',
     headerName: 'Company Name',
-    align: 'center',
-    headerAlign: 'center',
-    width: 270
+    width: 250
   },
   {
     field: 'role',
     headerName: 'Role',
-    align: 'center',
-    headerAlign: 'center',
-    width: 270
+    width: 200
   },
   {
     field: 'deadline',
     headerName: 'Application Deadline',
-    align: 'center',
-    headerAlign: 'center',
     width: 270
   },
   {
     field: 'resume',
     headerName: 'Applied Resume',
-    align: 'center',
-    headerAlign: 'center',
+    sortable: false,
     width: 270,
   },
 ];
 const rows = [
-  { id: "Google", role: 'Software Dev', deadline: '12:00AM 31 May 2022', resume: "ID:2 Page Software Dev " },
-  { id: "Microsoft", role: 'Software Dev', deadline: '12:00AM 31 May 2022', resume: "ID:1 Page Software Dev" }
+  { id:1,companyName: "Google", role: 'Software Dev', deadline: '12:00AM 31 May 2022', resume: "ID:2 Page Software Dev " },
+  { id:2,companyName: "Microsoft", role: 'Software Dev', deadline: '12:00AM 31 May 2022', resume: "ID:1 Page Software Dev" }
 ];
 const applicationParams = {
   "applications": rows.length,
@@ -45,18 +43,16 @@ function Applications() {
     <>
       <div className={styles.container}>
         <Meta title="Applications - Intern Season" />
+        <Grid container spacing={1} direction={{xs:'column', md:'row'}} alignItems={{xs:'flex-start', md:'center'}}>
+          <Grid item xs={12} md={6}>
+            <h1>Your Applications</h1>
+          </Grid>
+          <Grid item xs={12} md={6} sx={sideTextStyle}>
+              <h2>Total Applications : {applicationParams.applications}</h2>
+          </Grid>
+        </Grid>
         <Stack>
-          <div style={{ height: 500, margin: '0px auto' }} className={styles.datagridApplication}>
-            <Grid container spacing={1}>
-              <Grid item xs={6}>
-                <h1>Your Applications</h1>
-              </Grid>
-              <Grid item xs={6} style={gridMain}>
-                <div style={gridDiv}>
-                  <p style={pStyle}>Total Applications : {applicationParams.applications}</p>
-                </div>
-              </Grid>
-            </Grid>
+          <div style={{ height: 500, margin: '10px auto' }} className={styles.datagridApplications}>
             <DataGrid
               rows={rows}
               columns={columns}
@@ -71,27 +67,8 @@ function Applications() {
 Applications.layout = "studentPhaseDashboard"
 export default Applications
 
-const gridMain = {
-  width: "100%",
-  padding: "0",
-  margin: "0",
-  alignContent: "right",
-  alignItems: "right",
-};
-const gridDiv = {
-  alignContent: "right",
-  alignItems: "right",
-  width: "100%",
-  height: "100%",
-  margin: "0",
-  padding: "0",
-};
-
-const pStyle = {
-  transform: "translate(65%, 60%)",
-  margin: "30px 0 0 0",
-  padding: "0",
-  right: "0%",
-  width: "100%",
-  fontSize: "1.10em"
-};
+const sideTextStyle={
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-end',
+}
