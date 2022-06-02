e72532cd9bf5d5f33f8a80525ff29600b3398179
Manas Gupta <mmanasggupta@gmail.com>
2022-06-02 23:12:31 +0530

    Stepper demo

---
+++ pages/companyStep.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CompanyStepper from '@components/Stepper/companyStepper'
+function companyStep() {
+  return (
+    <div className='container'>
+      <CompanyStepper/>
+    </div>
+  )
+}
+
+companyStep.layout="studentPhaseDashboard"
+export default companyStep
