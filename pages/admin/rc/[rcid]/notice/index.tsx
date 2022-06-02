0bb02ebd188690d08c2782051333d432a3cc883e
amandeep <samandeep21@iitk.ac.in>
2022-06-02 12:34:28 +0530

    updated notice tables

--- pages/admin/rc/[rcid]/notice/index.tsx
+++ pages/admin/rc/[rcid]/notice/index.tsx
@@ -1,15 +1,55 @@
-import { useRouter } from 'next/router';
-import React from 'react'
-import Meta from '../../../../../components/Meta';
+import { Stack, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
+import Meta from '../../../../../components/Meta'
+import styles from '../../../../..//styles/PhaseOverview.module.css'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Link from 'next/link';
+import ActiveButton from '../../../../../components/Buttons/ActiveButton';
+const columns: GridColDef[] = [
+  {
+    field: 'id',
+    headerName: '',
+    width: 1100,
+  },
+  {
+    field: 'publishedDateAndTime',
+    headerName: 'Published Date And Time',
+    width: 300,
+  },
+  
+];
 
-function Index() {
+const rows = [
+  { id:'Company Name : Title', publishedDateAndTime : 'May 26 2019' },
+];
+
+const Index = () => {
   return (
-    <div className="container">
-      <Meta title="Notices - Admin"/>
-      <h1>Notices</h1>
+    <div className={styles.container}>
+      <Meta title="Notices" />
+      <Stack>
+        <h1>Internship 2022-23 Phase 1</h1>
+        <Stack 
+          direction="row"
+          marginRight={5}
+          marginLeft={5}
+          alignItems="left"
+          justifyContent="space-between"
+        >
+          <h2>Notices</h2>
+          <Link href='../:id/notice/new'><AddIcon/></Link>
+        </Stack>
+        <div style={{ height: 600,width : 1400, margin: '0px auto' }} className={styles.datagridOverView}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={7}
+            />
+        </div>
+      </Stack>
     </div>
   )
 }
 
-Index.layout="adminPhaseDashBoard";
+Index.layout = 'adminPhaseDashBoard'
 export default Index
--- pages/admin/rc/[rcid]/notice/new.tsx
+++ pages/admin/rc/[rcid]/notice/new.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NewNotice() {
   return (
     <div>
-      
+      <h1>new Notice</h1>
     </div>
   )
 }
