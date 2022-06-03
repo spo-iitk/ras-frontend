3768077cd3fc7589314af21e66cbde4d2d76666b
amandeep <samandeep21@iitk.ac.in>
2022-06-03 16:03:58 +0530

    updated enroll,freeze,unfreeze forms in admin/rc/:id/student

--- pages/admin/rc/[rcid]/student/unfreeze.tsx
+++ pages/admin/rc/[rcid]/student/unfreeze.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import FormControl from '@mui/material/FormControl';
+import { Button, FormHelperText, Input, InputLabel, Stack, TextField, Typography } from '@mui/material';
+import ActiveButton from '../../../../../components/Buttons/ActiveButton';
+import InactiveButton from '../../../../../components/Buttons/InactiveButton';
 
-function Unfreeze() {
+
+const h1="UNFREEZE (GROUP)"
+function UNFREEZE() {
   return (
     <div>
-      
+      <Stack
+        style={{textAlign:'left',marginBottom:150}} 
+      >
+        <h1>INTERNSHIP 2022-23 PHASE 1</h1>
+      </Stack>
+      <h1 style={{textAlign:'center'}}>{h1}</h1>
+      <FormControl>
+        <Stack
+          style={{alignItems:'center',marginRight: 100, marginLeft: 400}}
+        >
+          <Typography>ENTER ROLL NUMBERS</Typography>
+          <TextField id="rollNo" variant="filled" fullWidth style={{ width: "700px" }} />
+          <Typography style={{textAlign:'center',marginTop:20,marginBottom:20}}>OR</Typography>
+        </Stack>
+        <Stack
+          style={{alignItems:'center',marginRight: 100, marginLeft: 400}}
+        >
+          <Typography>ENTER EMAIL IDS</Typography>
+          <TextField id="emailId" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+          style={{alignItems:'center',marginRight: 100, marginLeft: 400,marginTop:80,justifyContent:'space-between'}}
+        >
+          <ActiveButton onClick={() => { location.href = '../notice' }}>UNFREEZE</ActiveButton>
+          <InactiveButton onClick={() => { location.href = '../notice/new' }}>RESET</InactiveButton>
+        </Stack>
+      </FormControl>
     </div>
   )
 }
 
-export default Unfreeze
+UNFREEZE.layout = 'adminPhaseDashBoard'
+export default UNFREEZE
