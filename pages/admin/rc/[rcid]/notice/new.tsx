8edc9db63797fd30e68ab2d5dbd7452d5c601e87
amdpt323 <95157589+amdpt323@users.noreply.github.com>
2022-06-05 09:28:55 +0530

    updated new notice , add.tsx in rcid / company and (#48)
    * updated new notice , add.tsx in rcid / company and
    index.tsx in pio-ppo
    
    * fixed @component meta in forgotpass
    
    * changed @component/

--- pages/admin/rc/[rcid]/notice/new.tsx
+++ pages/admin/rc/[rcid]/notice/new.tsx
@@ -1,11 +1,59 @@
 import React from 'react'
+import FormControl from '@mui/material/FormControl';
+import { Button, FormHelperText, Input, InputLabel, Stack, TextField } from '@mui/material';
+import ActiveButton from '@components/Buttons/ActiveButton';
+import InactiveButton from '@components/Buttons/InactiveButton';
 
 function NewNotice() {
   return (
     <div>
-      <h1>new Notice</h1>
+      <h1 style={{ textAlign: 'center' }}>ADD NOTICE</h1>
+      <FormControl>
+        <Stack
+          direction='row'
+        >
+          <h2 style={{ marginRight: 100, marginLeft: 100 }}>COMPANY NAME</h2>
+          <TextField id="companyName" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+        >
+          <h2 style={{ marginRight: 192, marginLeft: 100 }}>SUBJECT</h2>
+          <TextField id="subject" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+        >
+          <h2 style={{ marginRight: 185, marginLeft: 100 }}>MESSAGE</h2>
+          <TextField id="message" variant="filled" multiline rows={4} style={{ width: "700px",marginBottom:"20px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+        >
+          <h2 style={{ marginRight: 135, marginLeft: 100 }}>ATTACHMENT</h2>
+          <Button
+            variant="contained"
+            component="label"
+            style={{width:"300px",backgroundColor:"gray"}}
+          >
+            Upload File
+            <input
+              type="file"
+              hidden
+            />
+          </Button>
+        </Stack>
+        <Stack
+          direction='row'
+          style={{marginTop:"140px",marginLeft:"100px",justifyContent:"space-between"}}
+        >
+          <ActiveButton onClick={()=>{location.href='../notice'}}>ADD</ActiveButton>
+          <InactiveButton onClick={()=>{location.href='../notice/new'}}>RESET</InactiveButton>
+        </Stack>
+      </FormControl>
     </div>
   )
 }
 
+NewNotice.layout = 'adminPhaseDashBoard'
 export default NewNotice
--- pages/admin/rc/[rcid]/pio-ppo/index.tsx
+++ pages/admin/rc/[rcid]/pio-ppo/index.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import FormControl from '@mui/material/FormControl';
+import { Button, FormHelperText, Input, InputLabel, Stack, TextField } from '@mui/material';
+import ActiveButton from '@components/Buttons/ActiveButton';
+import InactiveButton from '@components/Buttons/InactiveButton';
 
 function Index() {
   return (
     <div>
-      
+      <Stack
+        style={{textAlign:'center',marginBottom:50}} 
+      >
+        <h1 >ADD PPO/PIO</h1>
+      </Stack>
+      <FormControl>
+        <Stack
+          style={{marginLeft:'350px'}}
+        >
+          <h2 style={{ marginRight: 96, textAlign:'left' }}>COMPANY NAME</h2>
+          <TextField id="companyName" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          style={{marginLeft:'350px'}}
+        >
+          <h2 style={{ marginRight: 96, textAlign:'left' }}>ENTER ROLL NUMBERS</h2>
+          <TextField id="rollNo" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <h2 style={{textAlign:"center"}}>OR</h2>
+        <Stack
+          style={{marginLeft:'350px'}}
+        >
+          <h2 style={{ marginRight: 96, textAlign:'left' }}>ENTER EMAIL IDS</h2>
+          <TextField id="emailId" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+          style={{ marginTop: "140px", marginLeft: "350px", justifyContent: "space-between" }}
+        >
+          <ActiveButton onClick={() => { location.href = '../notice' }}>ADD</ActiveButton>
+          <InactiveButton onClick={() => { location.href = '../notice/new' }}>RESET</InactiveButton>
+        </Stack>
+      </FormControl>
     </div>
   )
 }
 
+Index.layout = 'adminPhaseDashBoard'
 export default Index
