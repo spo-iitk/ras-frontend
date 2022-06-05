65d7738d289e8f3f856b159adbaf3d24ab64968b
Krishnansh5 <5272.krishnansh@gmail.com>
2022-06-05 16:15:12 +0530

    Hydration error solved on both PRs

--- pages/admin/rc/[rcid]/company/[companyId]/index.tsx
+++ pages/admin/rc/[rcid]/company/[companyId]/index.tsx
@@ -1,11 +1,128 @@
 import React from 'react'
+import Meta from '../../../../../../components/Meta';
+import ActiveButton from '../../../../../../components/Buttons/ActiveButton';
+import styles from '../../../../../..//styles/adminPhase.module.css'
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { IconButton, TableHead } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import AddIcon from '@mui/icons-material/Add';
 
+const info = [
+  {
+    field: 'Name',
+    value: 'XYZ'
+  },
+  {
+    field: 'Email',
+    value: 'XYZ@company.com'
+  },
+  {
+    field: 'Phone number',
+    value: '+91 6969696969'
+  },
+  {
+    field: 'Username',
+    value: 'XYZ@12'
+  }
+]
+
+const columns: GridColDef[] = [
+  {
+    field: 'designation',
+    headerName: 'Designation',
+    width: 200,
+  },
+ {
+   field: 'activeApplicants',
+   headerName: 'Active applicants',
+   width: 200,
+ },
+ {
+   field: 'totalApplicants',
+   headerName: 'Total applicants',
+   width: 200,
+ },
+ {
+   field: 'status',
+   headerName: 'Status',
+   width: 200,
+ },
+ {
+   field: 'deadline',
+   headerName: 'Deadline',
+   width: 200,
+ },
+];
+
+const rows = [
+  {id:1, designation:'Role 1',activeApplicants: '102',totalApplicants: '105',status: 'Accepted',deadline: 'May 26,2019'},
+ ];
 function Index() {
   return (
-    <div>
-      
+    <div style={{textAlign:'center', padding:'1rem'}}>
+      <Meta title='Company Dashboard'></Meta>
+      <h1>COMPANY NAME</h1>
+      <div style={ButtonContainer}>
+        <ActiveButton>VIEW COMPANY HISTORY</ActiveButton>
+        <ActiveButton>CONTACT DETAILS</ActiveButton>
+        <ActiveButton>PAST HIRES</ActiveButton>
+        <ActiveButton>ADD PPO/PIIO</ActiveButton>
+      </div>
+      <br/>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell sx={{p:2,fontWeight:600,fontSize:'1.5rem'}}>
+              Active Contact HR Details
+              </TableCell>
+            </TableRow>
+            {
+              info.map((item)=>(
+                <TableRow>
+                  <TableCell>
+                    {item.field}
+                  </TableCell>
+                  <TableCell>
+                    {item.value}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <br />
+      <br />
+      <Grid container spacing={2} component={Paper}>
+        <Grid item xs={11}>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+        <h2>Internship Roles</h2>
+        <IconButton><AddIcon /></IconButton>
+        </div>  
+        </Grid>
+        <Grid item xs={11} style={{ height: 500, margin: '20px auto' }} className={styles.datagridBranch}>
+          <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={7}
+              rowsPerPageOptions={[7]}
+          />
+        </Grid>
+      </Grid>
     </div>
   )
 }
 
+Index.layout = 'studentDashboard'
 export default Index
+
+const ButtonContainer = {
+  display: 'flex',
+  justifyContent: 'space-around'
+}
--- pages/admin/rc/new.tsx
+++ pages/admin/rc/new.tsx
@@ -1,11 +1,183 @@
+import { FormControl, Stack } from '@mui/material'
 import React from 'react'
+import styled from '@emotion/styled';
+import { Button, ButtonProps } from '@mui/material';
+import ActiveButton from '../../../components/Buttons/ActiveButton';
+import { green, red } from '@mui/material/colors';
+import { TextField } from '@mui/material';
+import Meta from '../../../components/Meta';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
 
-function NewRC() {
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Link from 'next/link';
+
+const info = [
+  {
+    field: 'Academic Year',
+    value: 'Select Academic Year',
+  },
+  {
+    field: 'Type of Recruitment',
+    value: 'Select type of recruitment',
+  },
+  {
+    field: 'Phase',
+    value: 'Select Phase',
+  }
+]
+
+const Policy = [
+  {
+    field: 'Max No. of Applications',
+    value: '',
+  }
+]
+const RecruitmentCycle = () => {
   return (
-    <div>
-      
+    <div style={{ padding: "0 2rem" }}>
+      <Meta title="Student Dashboard - Create New Recruitment Cycle" />
+      <Stack spacing={2}>
+        <div style={{textAlign:'center', fontSize:'1.5rem'}}>
+        <h1>Create New Recruitment Cycle</h1>
+        </div>
+        <Stack spacing={2} justifyContent="center" alignItems="center">
+          <TableContainer component={Paper}>
+            <Table>
+              <TableBody>
+                {info.map((item, index) => (
+                  <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    key={index}
+                  >
+                    <TableCell align='left' component="th" scope="row" sx={{ fontSize: "1.5em", fontWeight: '600', paddingLeft: '7vw' }}>
+                      {item.field}:
+                    </TableCell>
+                    <TableCell align='left' sx={{ fontSize: "1.5em", padding: 0 }}>
+                      <TextField
+                        id={item.field}
+                        variant="outlined"
+                        value={item.value}
+                        size="small"
+                        sx={{ m: 3, width: { xs: '30ch', sm: '27ch', lg: '35ch' }, padding:0 }} />
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell>
+                    <p style={{ fontSize: "1.7em", fontWeight: '600', paddingLeft: '7vw' }}>Policies:</p>
+                    {Policy.map((item,index)=>(
+                      <ol>
+                        <li style={{display:'flex'}}>
+                          <p style={{ fontSize:'1.7em', paddingLeft:'7vw'}}>{item.field}</p>
+                          <TextField
+                          id="Max no. of Applications"
+                          value={item.value}
+                          variant="outlined"
+                          size="small"
+                          sx={{ m: 3, width: { xs: '30ch', sm: '27ch', lg: '35ch' }, padding:0 }} /> 
+                        </li>
+                      </ol>
+                    ))}
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <p style={{ fontSize:'1.7em',fontWeight: '600', paddingLeft:'7vw'}}>Additional Questions:</p>
+                    <br />
+                    <br/>
+                    <div style={{alignItems:'center',marginLeft: '10vw'}}>  
+                    <TableContainer sx={{ minWidth: 300, maxWidth: 700}}>
+                      <Table sx={{'& td':{border:0}}}>
+                        <TableBody>
+                          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell>
+                              QUESTION TYPE
+                            </TableCell>
+                            <TableCell>
+                              <TextField 
+                              id="Question Type"
+                              variant="outlined"
+                              size="small"
+                              sx={{ m: 3, width: { xs: '40ch', sm: '35ch', lg: '50ch' }, padding:0 }}
+                              >
+                              </TextField>
+                            </TableCell>
+                          </TableRow>  
+                          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell>
+                              QUESTION
+                            </TableCell>
+                            <TableCell>
+                              <TextField
+                              id="Question"
+                              variant="outlined"
+                              size="small"
+                              sx={{ m: 3, width: { xs: '40ch', sm: '35ch', lg: '50ch' }, padding:0 }}
+                              >
+                              </TextField>
+                            </TableCell>
+                          </TableRow>
+                          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell>
+                              OPTION 1
+                            </TableCell>
+                            <TableCell>
+                              <TextField
+                              id="Opt1"
+                              variant="outlined"
+                              size="small"
+                              sx={{ m: 3, width: { xs: '30ch', sm: '27ch', lg: '35ch' }, padding:0 }}
+                              >
+                              </TextField>
+                            </TableCell>
+                            </TableRow>
+                          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell>
+                              OPTION 2
+                            </TableCell>
+                            <TableCell>
+                              <TextField
+                              id="Opt1"
+                              variant="outlined"
+                              size="small"
+                              sx={{ m: 3, width: { xs: '30ch', sm: '27ch', lg: '35ch' }, padding:0 }}
+                              >
+                              </TextField>
+                            </TableCell>
+                          </TableRow>  
+                          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell>
+                              OPTION 3
+                            </TableCell>
+                            <TableCell>
+                              <TextField
+                              id="Opt1"
+                              variant="outlined"
+                              size="small"
+                              sx={{ m: 3, width: { xs: '30ch', sm: '27ch', lg: '35ch' }, padding:0 }}
+                              >
+                              </TextField>
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <ActiveButton sx={{width:'max-content'}}>CREATE RECRUITMENT DRIVE</ActiveButton>
+        </Stack>
+      </Stack>
     </div>
   )
 }
 
-export default NewRC
+RecruitmentCycle.layout = 'studentDashboard'
+export default RecruitmentCycle
