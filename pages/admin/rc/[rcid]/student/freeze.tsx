1436fa183576634650890a809cd09b331b8ac59b
amdpt323 <95157589+amdpt323@users.noreply.github.com>
2022-06-05 09:39:06 +0530

    Add forms (#51)
    * updated new notice , add.tsx in rcid / company and
    index.tsx in pio-ppo
    
    * fixed @component meta in forgotpass
    
    * updated enroll,freeze,unfreeze forms in
    admin/rc/:id/student
    
    * responsiveness fixed
    
    Co-authored-by: Manas Gupta <mmanasggupta@gmail.com>

--- pages/admin/rc/[rcid]/student/freeze.tsx
+++ pages/admin/rc/[rcid]/student/freeze.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import FormControl from '@mui/material/FormControl';
+import { Button, FormHelperText, Input, InputLabel, Stack, TextField, Typography } from '@mui/material';
+import ActiveButton from '../../../../../components/Buttons/ActiveButton';
+import InactiveButton from '../../../../../components/Buttons/InactiveButton';
 
+
+const h1="FREEZE (GROUP)"
 function Freeze() {
   return (
     <div>
-      
+      <Stack
+        style={{textAlign:'left',marginBottom:150}} 
+      >
+        <h1>INTERNSHIP 2022-23 PHASE 1</h1>
+      </Stack>
+      <h1 style={{textAlign:'center'}}>{h1}</h1>
+      <FormControl style={{display:"flex",justifyItems:'center'}}>
+        <Stack
+          style={{alignItems:'center',margin:"auto"}}
+        >
+          <Typography>ENTER ROLL NUMBERS</Typography>
+          <TextField id="rollNo" variant="filled" fullWidth style={{ width: "700px" }} />
+          <Typography style={{textAlign:'center',marginTop:20,marginBottom:20}}>OR</Typography>
+        </Stack>
+        <Stack
+          style={{alignItems:'center',margin:"auto"}}
+        >
+          <Typography>ENTER EMAIL IDS</Typography>
+          <TextField id="emailId" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+          style={{alignItems:'center',marginTop:60,justifyContent:'space-evenly'}}
+        >
+          <ActiveButton onClick={() => { location.href = '../notice' }}>Freeze</ActiveButton>
+          <InactiveButton onClick={() => { location.href = '../notice/new' }}>RESET</InactiveButton>
+        </Stack>
+      </FormControl>
     </div>
   )
 }
 
+Freeze.layout = 'adminPhaseDashBoard'
 export default Freeze
--- pages/admin/rc/[rcid]/student/unfreeze.tsx
+++ pages/admin/rc/[rcid]/student/unfreeze.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
+import FormControl from '@mui/material/FormControl';
+import { Button, FormHelperText, Input, InputLabel, Stack, TextField, Typography } from '@mui/material';
+import ActiveButton from '../../../../../components/Buttons/ActiveButton';
+import InactiveButton from '../../../../../components/Buttons/InactiveButton';
 
+
+const h1="UFREEZE (GROUP)"
 function Unfreeze() {
   return (
     <div>
-      
+      <Stack
+        style={{textAlign:'left',marginBottom:150}} 
+      >
+        <h1>INTERNSHIP 2022-23 PHASE 1</h1>
+      </Stack>
+      <h1 style={{textAlign:'center'}}>{h1}</h1>
+      <FormControl style={{display:"flex",justifyItems:'center'}}>
+        <Stack
+          style={{alignItems:'center',}}
+        >
+          <Typography>ENTER ROLL NUMBERS</Typography>
+          <TextField id="rollNo" variant="filled" fullWidth style={{ width: "700px" }} />
+          <Typography style={{textAlign:'center',marginTop:20,marginBottom:20}}>OR</Typography>
+        </Stack>
+        <Stack
+          style={{alignItems:'center',}}
+        >
+          <Typography>ENTER EMAIL IDS</Typography>
+          <TextField id="emailId" variant="filled" fullWidth style={{ width: "700px" }} />
+        </Stack>
+        <Stack
+          direction='row'
+          style={{alignItems:'center',marginTop:60,justifyContent:'space-evenlyc'}}
+        >
+          <ActiveButton onClick={() => { location.href = '../notice' }}>Unfreeze</ActiveButton>
+          <InactiveButton onClick={() => { location.href = '../notice/new' }}>RESET</InactiveButton>
+        </Stack>
+      </FormControl>
     </div>
   )
 }
 
+Unfreeze.layout = 'adminPhaseDashBoard'
 export default Unfreeze
