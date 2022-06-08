63fa458c50b3a76815602b99211c634f2b337da4
Manas Gupta <mmanasggupta@gmail.com>
2022-06-08 16:17:38 +0530

    Fix responsiveness

--- pages/admin/company/index.tsx
+++ pages/admin/company/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import Grid from "@mui/material/Grid";
-import { Card, IconButton, Stack } from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import styles from "@styles/adminPhase.module.css";
 import ActiveButton from "@components/Buttons/ActiveButton";
@@ -9,6 +9,7 @@ import Meta from "@components/Meta";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 100 },
   {
     field: "CompanyName",
     headerName: "Company Name",
@@ -22,9 +23,14 @@ const columns: GridColDef[] = [
   {
     field: "ViewDetails",
     headerName: "View Details",
-    width: 400,
+    width: 200,
     renderCell: () => (
-      <Stack direction="row" spacing={18}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        width="100%"
+        justifyContent="space-between"
+      >
         <ActiveButton sx={{ height: 30 }}>CLICK HERE</ActiveButton>
         <IconButton>
           <MoreVertIcon />
@@ -41,48 +47,44 @@ const rows = [
     Tags: "Core,PSU,Analytics,Software,Startup,Noncore",
     ViewDetails: "Click Here",
   },
+  {
+    id: 2,
+    CompanyName: "Company 1",
+    Tags: "Core,PSU,Analytics,Software,Startup,Noncore",
+    ViewDetails: "Click Here",
+  },
 ];
 
 function Index() {
   return (
     <div className={styles.container}>
       <Meta title="Master Company Database" />
-      <Card
-        elevation={2}
-        sx={{
-          padding: 3,
-          width: { xs: "100%", sm: "100%", margin: "0px auto" },
-        }}
-      >
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12}>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <h1>Master Database (Comapny)</h1>
-              <Stack direction="row" spacing={3}>
-                <IconButton>
-                  <AddIcon />
-                </IconButton>
-                <IconButton>
-                  <MoreVertIcon />
-                </IconButton>
-              </Stack>
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <div
-              className={styles.datagridCompany}
-              style={{ height: 500, margin: "20px auto" }}
-            >
-              <DataGrid
-                rows={rows}
-                columns={columns}
-                pageSize={7}
-                rowsPerPageOptions={[7]}
-              />
-            </div>
-          </Grid>
+      <Grid container alignItems="center">
+        <Grid item xs={12}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h1>Master Database (Comapny)</h1>
+            <Stack direction="row" spacing={3}>
+              <IconButton>
+                <AddIcon />
+              </IconButton>
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            </Stack>
+          </div>
         </Grid>
-      </Card>
+        <div
+          className={styles.datagridMasterCompany}
+          style={{ height: 500, margin: "0px auto" }}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={7}
+            rowsPerPageOptions={[7]}
+          />
+        </div>
+      </Grid>
     </div>
   );
 }
