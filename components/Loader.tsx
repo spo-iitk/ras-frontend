d0c111b4a7f64dfcf3252beb6fd6c81d7121d5ad
ahujatejas06 <ahujatejas06@gmail.com>
2022-06-08 22:01:02 +0530

    Hoping to pass ADVANCED deployment test

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,26 +1,49 @@
-
+import React from "react";
 import loaderStyles from "../styles/Loader.module.css";
 
-function Loader()
-{
-    return (
-      <div className={loaderStyles.card}>
-        <div className={loaderStyles.p32}>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.titleLine} ${loaderStyles.end}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.titleLine} ${loaderStyles.end}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.titleLine} `}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-            <div className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}></div>
-        </div>
+function Loader() {
+  return (
+    <div className={loaderStyles.card}>
+      <div className={loaderStyles.p32}>
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.titleLine} ${loaderStyles.end}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.titleLine} ${loaderStyles.end}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.titleLine} `}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
+        <div
+          className={`${loaderStyles.shimmerBG} ${loaderStyles.contentLine} ${loaderStyles.mt24}`}
+        />
       </div>
-    )
+    </div>
+  );
 }
 Loader.layout = "studentPhaseDashboard";
 export default Loader;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,31 @@
+import { Router } from "next/router";
 import React, { useState } from "react";
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import LayoutWrapper from "@components/Layouts/LayoutWrapper";
-import { Router, useRouter } from "next/router";
-import loaderStyles from "../styles/Loader.module.css";
 import Loader from "@components/Loader";
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
-  Router.events.on("routeChangeStart", (url)=>{
+  Router.events.on("routeChangeStart", () => {
     setLoading(true);
   });
-  Router.events.on("routeChangeComplete", (url)=>{
+  Router.events.on("routeChangeComplete", () => {
     setLoading(false);
   });
   return (
     <>
-      {loading && <LayoutWrapper>
+      {loading && (
+        <LayoutWrapper>
           <Loader />
-        </LayoutWrapper>}
+        </LayoutWrapper>
+      )}
 
-    {!loading && <LayoutWrapper>
-      <Component {...pageProps} />
-    </LayoutWrapper>}
+      {!loading && (
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      )}
     </>
   );
 }
