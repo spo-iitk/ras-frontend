4706b4628b92ce2a43486716912cce034e7389c2
Manas Gupta <mmanasggupta@gmail.com>
2022-06-09 10:17:15 +0530

    Add proforma page in student side

--- pages/student/rc/[rcId]/notices.tsx
+++ pages/student/rc/[rcId]/notices.tsx
@@ -1,10 +1,50 @@
-import React from "react";
 import Meta from "@components/Meta";
+import { Stack } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import styles from "@styles/adminPhase.module.css";
+import React from "react";
+
+const columns: GridColDef[] = [
+  {
+    field: "id",
+    headerName: "Id",
+    width: 100,
+  },
+  {
+    field: "name",
+    headerName: "Company Name",
+    width: 300,
+  },
+  {
+    field: "publishedDateAndTime",
+    headerName: "Published Date And Time",
+    width: 200,
+  },
+];
+
+const rows = [
+  { id: 1, name: "Company Name : Title", publishedDateAndTime: "May 26 2019" },
+];
 
 function Notices() {
   return (
-    <div>
+    <div className={styles.container}>
       <Meta title="Notices - Intern Season" />
+      <Stack>
+        <h1>Internship 2022-23 Phase 1</h1>
+        <h2>Notices</h2>
+        <div
+          style={{ height: 500, margin: "0px auto" }}
+          className={styles.datagridNotices}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={7}
+            rowsPerPageOptions={[7]}
+          />
+        </div>
+      </Stack>
     </div>
   );
 }
--- pages/student/rc/[rcId]/proforma/[proformaId].tsx
+++ pages/student/rc/[rcId]/proforma/[proformaId].tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "@styles/internPhase.module.css";
 
 function Proforma() {
-  return <div />;
+  return (
+    <div className={styles.container}>
+      <h1>Proforma</h1>
+    </div>
+  );
 }
 
 Proforma.layout = "studentPhaseDashboard";
