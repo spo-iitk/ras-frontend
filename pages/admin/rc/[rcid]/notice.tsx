b0b3d39b8ea11121e34fbb8f321ccef87655595c
Manas Gupta <mmanasggupta@gmail.com>
2022-06-10 18:53:12 +0530

    Make modal component for add notice and removed notice/new

---
+++ pages/admin/rc/[rcid]/notice.tsx
@@ -0,0 +1,77 @@
+import Meta from "@components/Meta";
+import NewNotice from "@components/Modals/newNotice";
+import AddIcon from "@mui/icons-material/Add";
+import { IconButton, Modal, Stack } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import styles from "@styles/adminPhase.module.css";
+import * as React from "react";
+
+const columns: GridColDef[] = [
+  {
+    field: "id",
+    headerName: "Id",
+    width: 100,
+  },
+  {
+    field: "name",
+    headerName: "Company Name",
+    width: 300,
+  },
+  {
+    field: "publishedDateAndTime",
+    headerName: "Published Date And Time",
+    width: 200,
+  },
+];
+
+const rows = [
+  { id: 1, name: "Company Name : Title", publishedDateAndTime: "May 26 2019" },
+];
+
+function Index() {
+  const [openNew, setOpenNew] = React.useState(false);
+  const handleOpenNew = () => {
+    setOpenNew(true);
+  };
+  const handleCloseNew = () => {
+    setOpenNew(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <Meta title="Notices" />
+      <Stack>
+        <h1>Internship 2022-23 Phase 1</h1>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <h2>Notices</h2>
+          <Stack direction="row" spacing={3}>
+            <IconButton onClick={handleOpenNew}>
+              <AddIcon />
+            </IconButton>
+          </Stack>
+        </Stack>
+        <div
+          style={{ height: 500, margin: "0px auto" }}
+          className={styles.datagridNotices}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={7}
+            rowsPerPageOptions={[7]}
+          />
+        </div>
+      </Stack>
+      <Modal open={openNew} onClose={handleCloseNew}>
+        <NewNotice handleCloseNew={handleCloseNew} />
+      </Modal>
+    </div>
+  );
+}
+
+Index.layout = "adminPhaseDashBoard";
+export default Index;
