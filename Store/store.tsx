68a7ff21ce11bca46b192c1b48d84d6b3da375cc
Manas Gupta <mmanasggupta@gmail.com>
2022-06-12 14:38:14 +0530

    Fix callbacks  tempalate and add zustand

---
+++ Store/store.tsx
@@ -0,0 +1,4 @@
+import create from "zustand";
+
+const useStore = create((set) => ({}));
+export default useStore;
--- callbacks/auth.tsx
+++ callbacks/auth.tsx
@@ -1,42 +1,91 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 import axios from "axios";
 import { AUTH_URL } from "./constants";
 
-type login_params = {
+interface login_params {
   user_id: string;
   password: string;
   remember_me: boolean;
-};
-type signup_params = {
+}
+interface signup_params {
   user_id: string;
   password: string;
   name: string;
   otp: string;
   roll_no: string;
-};
+}
 
-export const login = async (data: login_params) => {
-  console.log(data);
-  console.log(AUTH_URL);
-  axios
+interface Response {
+  Payload: any;
+  Status: any;
+  Token?: any;
+}
+
+const login = async (data: login_params): Promise<Response> => {
+  let payload;
+  let status;
+  await axios
     .post(`${AUTH_URL}/login`, { params: data })
-    .then((res) => console.log(res.data?.status))
-    .catch((err) => console.log(err));
+    .then((res) => {
+      payload = res.data;
+      status = res?.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error;
+      status = err?.response?.status;
+    });
+
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+
+  return response;
 };
 
-export const signup = async (data: signup_params) => {
-  console.log(data);
-  axios
+const signup = async (data: signup_params): Promise<Response> => {
+  let payload;
+  let status;
+  await axios
     .post(`${AUTH_URL}/signup`, { params: data })
-    .then((res) => console.log(res.data?.status))
-    .catch((err) => console.log(err));
+    .then((res) => {
+      payload = res.data;
+      status = res?.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error;
+      status = err?.response?.status;
+    });
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+
+  return response;
 };
 
-export const otp = async (user_id: string) => {
+const otp = async (user_id: string): Promise<Response> => {
+  let payload;
+  let status;
   await axios
     .post(`${AUTH_URL}/otp`, { user_id })
-    .then((res) => console.log(res.data?.status))
-    .catch((err) => console.log(err));
+    .then((res) => {
+      payload = res.data;
+      status = res?.status;
+    })
+    .catch((err) => {
+      payload = err?.response?.data.error;
+      status = err?.response?.status;
+    });
+  const response: Response = {
+    Payload: payload,
+    Status: status,
+  };
+
+  return response;
 };
-export default { login, signup, otp };
+
+export { login, signup, otp };
+export type { login_params, signup_params, Response };
