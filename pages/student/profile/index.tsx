492e39e1e86b69bdb71fcde69a96ea32837ae4fa
bangaradi <adityavb@cse.iitk.ac.in>
2022-06-16 18:36:36 +0530

    fixed errors, adding api-fetched  data to student profile

--- pages/student/profile/index.tsx
+++ pages/student/profile/index.tsx
@@ -2,177 +2,219 @@ import Meta from "@components/Meta";
 import { Card, Grid, Stack, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import studentRequest, { Student } from "@callbacks/student/student";
 
-const info = [
-  {
-    field: "Name",
-    value: "Enter your Name",
-    disabled: true,
-  },
-  {
-    field: "Program",
-    value: "Select your Program",
-    disabled: true,
-  },
-  {
-    field: "Department",
-    value: "Select your Department",
-    disabled: true,
-  },
-  {
-    field: "Specialisation",
-    value: "Enter your Specialisation",
-    disabled: false,
-  },
-  {
-    field: "IITK Roll No.",
-    value: "Enter your IITK Roll No.",
-    disabled: false,
-  },
-  {
-    field: "Preference",
-    value: "Select your Preference",
-    disabled: false,
-  },
-  {
-    field: "Gender",
-    value: "Select your Gender",
-    disabled: false,
-  },
-  {
-    field: "Disability",
-    value: "Select your Disability Status",
-    disabled: false,
-  },
-  {
-    field: "DOB",
-    value: "Enter your Date of Birth",
-    disabled: false,
-  },
-  {
-    field: "Expected Graduation Year",
-    value: "Select your Graduation Year",
-    disabled: false,
-  },
-  {
-    field: "IITK Email",
-    value: "Your IITK email",
-    disabled: false,
-  },
-  {
-    field: "Personal Email",
-    value: "Enter your Personal Email",
-    disabled: false,
-  },
-  {
-    field: "Contact Number",
-    value: "Enter your Contact Number",
-    disabled: false,
-  },
-  {
-    field: "Alternate Contact Numer",
-    value: "Enter your Alternate Contact Number",
-    disabled: false,
-  },
-  {
-    field: "Whatsapp Number",
-    value: "Enter your Whatsapp Number",
-    disabled: false,
-  },
-  {
-    field: "Current CPI",
-    value: "Enter your Current CPI",
-    disabled: false,
-  },
-  {
-    field: "UG CPI(on for PG Students)",
-    value: "Enter your UG CPI",
-    disabled: false,
-  },
-  {
-    field: "10th Board",
-    value: "Enter your 10th Board Name",
-    disabled: false,
-  },
-  {
-    field: "10th Board Year",
-    value: "Enter your 10th Board Year",
-    disabled: false,
-  },
-  {
-    field: "10th Marks",
-    value: "Enter your 10th Marks",
-    disabled: false,
-  },
-  {
-    field: "12th Board",
-    value: "Enter your 12th Board",
-    disabled: false,
-  },
-  {
-    field: "12th Board",
-    value: "Enter your 12th Board Name",
-    disabled: false,
-  },
-  {
-    field: "12th Board Year",
-    value: "Enter your 12th Board Year",
-    disabled: false,
-  },
-  {
-    field: "12th Board Marks",
-    value: "Enter your 12th Board Marks",
-    disabled: false,
-  },
-  {
-    field: "12th Board Year",
-    value: "Enter your 12th Board Year",
-    disabled: false,
-  },
-  {
-    field: "Entrance Exam",
-    value: "Enter your Entrance Exam",
-    disabled: false,
-  },
-  {
-    field: "Entrance Exam Rank",
-    value: "Enter your Entrance Exam Rank",
-    disabled: false,
-  },
-  {
-    field: "Category",
-    value: "Enter your Category",
-    disabled: false,
-  },
-  {
-    field: "Category Rank",
-    value: "Enter your Category Rank",
-    disabled: false,
-  },
-  {
-    field: "Current Address",
-    value: "Enter your Current Address",
-    disabled: false,
-  },
-  {
-    field: "Permanent Address",
-    value: "Enter your Permanent Address",
-    disabled: false,
-  },
-  {
-    field: "Friends Name",
-    value: "Enter your Friends Name",
-    disabled: false,
-  },
-  {
-    field: "Friends Contact Details",
-    value: "Enter your Friends Contace Details",
-    disabled: false,
-  },
-];
+const info: { field: string; value: string; disabled: boolean; api_id: any }[] =
+  [
+    {
+      field: "Name",
+      value: "Enter your Name",
+      disabled: true,
+      api_id: "name",
+    },
+    {
+      field: "Program",
+      value: "Select your Program",
+      disabled: true,
+      api_id: "program",
+    },
+    {
+      field: "Department",
+      value: "Select your Department",
+      disabled: true,
+      api_id: "department",
+    },
+    {
+      field: "Specialisation",
+      value: "Enter your Specialisation",
+      disabled: false,
+      api_id: "specialization",
+    },
+    {
+      field: "IITK Roll No.",
+      value: "Enter your IITK Roll No.",
+      disabled: false,
+      api_id: "roll_no",
+    },
+    {
+      field: "Preference",
+      value: "Select your Preference",
+      disabled: false,
+      api_id: "preference",
+    },
+    {
+      field: "Gender",
+      value: "Select your Gender",
+      disabled: false,
+      api_id: "gender",
+    },
+    {
+      field: "Disability",
+      value: "Select your Disability Status",
+      disabled: false,
+      api_id: "disability",
+    },
+    {
+      field: "DOB",
+      value: "Enter your Date of Birth",
+      disabled: false,
+      api_id: "dob",
+    },
+    {
+      field: "Expected Graduation Year",
+      value: "Select your Graduation Year",
+      disabled: false,
+      api_id: "expected_graduation_year",
+    },
+    {
+      field: "IITK Email",
+      value: "Your IITK email",
+      disabled: false,
+      api_id: "iitk_email",
+    },
+    {
+      field: "Personal Email",
+      value: "Enter your Personal Email",
+      disabled: false,
+      api_id: "personal_email",
+    },
+    {
+      field: "Contact Number",
+      value: "Enter your Contact Number",
+      disabled: false,
+      api_id: "phone",
+    },
+    {
+      field: "Alternate Contact Numer",
+      value: "Enter your Alternate Contact Number",
+      disabled: false,
+      api_id: "alternate_phone",
+    },
+    {
+      field: "Whatsapp Number",
+      value: "Enter your Whatsapp Number",
+      disabled: false,
+      api_id: "whatsapp_number",
+    },
+    {
+      field: "Current CPI",
+      value: "Enter your Current CPI",
+      disabled: false,
+      api_id: "current_cpi",
+    },
+    {
+      field: "UG CPI(on for PG Students)",
+      value: "Enter your UG CPI",
+      disabled: false,
+      api_id: "ug_cpi",
+    },
+    {
+      field: "10th Board",
+      value: "Enter your 10th Board Name",
+      disabled: false,
+      api_id: "tenth_board",
+    },
+    {
+      field: "10th Board Year",
+      value: "Enter your 10th Board Year",
+      disabled: false,
+      api_id: "tenth_year",
+    },
+    {
+      field: "10th Marks",
+      value: "Enter your 10th Marks",
+      disabled: false,
+      api_id: "tenth_marks",
+    },
+    {
+      field: "12th Board",
+      value: "Enter your 12th Board Name",
+      disabled: false,
+      api_id: "twelfth_board",
+    },
+    {
+      field: "12th Board Year",
+      value: "Enter your 12th Board Year",
+      disabled: false,
+      api_id: "twelfth_year",
+    },
+    {
+      field: "12th Board Marks",
+      value: "Enter your 12th Board Marks",
+      disabled: false,
+      api_id: "twelfth_marks",
+    },
+    {
+      field: "Entrance Exam",
+      value: "Enter your Entrance Exam",
+      disabled: false,
+      api_id: "entrance_exam",
+    },
+    {
+      field: "Entrance Exam Rank",
+      value: "Enter your Entrance Exam Rank",
+      disabled: false,
+      api_id: "entrance_exam_rank",
+    },
+    {
+      field: "Category",
+      value: "Enter your Category",
+      disabled: false,
+      api_id: "category",
+    },
+    {
+      field: "Category Rank",
+      value: "Enter your Category Rank",
+      disabled: false,
+      api_id: "category_rank",
+    },
+    {
+      field: "Current Address",
+      value: "Enter your Current Address",
+      disabled: false,
+      api_id: "current_address",
+    },
+    {
+      field: "Permanent Address",
+      value: "Enter your Permanent Address",
+      disabled: false,
+      api_id: "permanent_address",
+    },
+    {
+      field: "Friends Name",
+      value: "Enter your Friends Name",
+      disabled: false,
+      api_id: "friend_name",
+    },
+    {
+      field: "Friends Contact Details",
+      value: "Enter your Friends Contace Details",
+      disabled: false,
+      api_id: "friend_phone",
+    },
+  ];
 
 function Profile() {
+  // let Students : Student = {ID: 0} as Student;
+
+  const [StudentData, setStudentData] = useState<Student>({ ID: 0 } as Student);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const token = sessionStorage.getItem("token") || "";
+      const student = await studentRequest.get(token).catch((err) => {
+        console.log(err);
+        return { ID: 0 } as Student;
+      });
+      if (student.ID !== 0) {
+        console.log(student);
+        // Students = student;
+      }
+      setStudentData(student);
+    };
+    fetch();
+  }, []);
   return (
     <div style={{ padding: "0 2rem" }}>
       <Meta title="Student Dashboard - Profile" />
@@ -216,10 +258,11 @@ function Profile() {
                   <p>{item.field}</p>
                   <TextField
                     fullWidth
-                    label={item.value}
+                    // label={item.value}
                     disabled
                     id="standard-basic"
                     variant="standard"
+                    value={StudentData[item.api_id as keyof Student]}
                   />
                 </Grid>
               ))}
