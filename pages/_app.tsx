1dae382069d6dcb344b7a43330e5355ddf456e17
Manas Gupta <mmanasggupta@gmail.com>
2022-06-17 18:57:02 +0530

    Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,17 @@ import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import LayoutWrapper from "@components/Layouts/LayoutWrapper";
 import { NotificationsProvider } from "@mantine/notifications";
+import { ThemeProvider } from "@mui/material";
+import theme from "../theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NotificationsProvider position="top-right" zIndex={2077}>
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
+      <ThemeProvider theme={theme}>
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
     </NotificationsProvider>
   );
 }
