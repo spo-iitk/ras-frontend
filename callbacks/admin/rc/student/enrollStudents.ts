7ff3865148e3d7916e78c99f33b5373303a88392
Aadarsh <aadarshshaw05@gmail.com>
2022-06-18 17:58:15 +0530

    Admin rc students API

--- callbacks/admin/rc/student/enrollStudents.ts
+++ callbacks/admin/rc/student/enrollStudents.ts
@@ -13,13 +13,54 @@ export interface Emails {
   email: string[];
 }
 
+export interface Student {
+  CreatedAt: string;
+  DeletedAt: string;
+  ID: number;
+  UpdatedAt: string;
+  comment: string;
+  cpi: number;
+  email: string;
+  is_frozen: boolean;
+  name: string;
+  program_department_id: number;
+  recruitment_cycle_id: number;
+  secondary_program_department_id: number;
+  student_id: number;
+  type: string;
+}
+// CreatedAt: "2022-06-18T09:25:45.965335Z"
+// DeletedAt: null
+// ID: 3
+// UpdatedAt: "2022-06-18T09:25:45.965335Z"
+// comment: ""
+// cpi: 0
+// email: "harshitr20@iitk.ac.in"
+// is_frozen: false
+// name: "Harshit Raj"
+// program_department_id: 0
+// recruitment_cycle_id: 1
+// secondary_program_department_id: 0
+// student_id: 7
+// type: "Available"
+
 const instance = axios.create({
   baseURL: ADMIN_RC_URL,
   timeout: 15000,
   timeoutErrorMessage: SERVER_ERROR,
 });
 
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
 const postEmails = {
+  getAllStudents: (token: string, rcid: string) =>
+    instance
+      .get<Student[]>(`/${rcid}/student`, setConfig(token))
+      .then(responseBody)
+      .catch((err: ErrorType) => {
+        errorNotification("Failed to get students", err.response?.data?.error);
+        return [] as Student[];
+      }),
   post: (token: string, rcid: string, body: Emails) =>
     instance
       .post<StatusResponse, AxiosResponse<StatusResponse, Emails>, Emails>(
@@ -28,13 +69,13 @@ const postEmails = {
         setConfig(token)
       )
       .then((res) => {
-        successNotification("Enrolled students", res.data.status);
+        successNotification("Students Enrolled successfully", res.data.status);
         return true;
       })
       .catch((err: ErrorType) => {
         errorNotification(
-          "Could not fetch data",
-          err.response?.data.error || err.message
+          "Failed to Enroll Students",
+          err.response?.data?.error
         );
         return false;
       }),
