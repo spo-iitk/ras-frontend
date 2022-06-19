33ee8d641aca9e83cefb4bc99025fb97799a9977
Aadarsh <aadarshshaw05@gmail.com>
2022-06-19 18:05:36 +0530

    Added popup to resume and attendance - Admin (#115)

--- pages/admin/rc/[rcid]/resume/index.tsx
+++ pages/admin/rc/[rcid]/resume/index.tsx
@@ -1,6 +1,6 @@
 import AddIcon from "@mui/icons-material/Add";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { IconButton, Modal, Stack } from "@mui/material";
+import { Button, IconButton, Modal, Popover, Stack } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import * as React from "react";
@@ -12,91 +12,12 @@ import Meta from "@components/Meta";
 import InactiveButton from "@components/Buttons/InactiveButton";
 import ActiveButton from "@components/Buttons/ActiveButton";
 
-const columns: GridColDef[] = [
-  { field: "resumeid", headerName: "Resume ID", width: 200 },
-  {
-    field: "StudentName",
-    headerName: "Student Name",
-    width: 200,
-  },
-  {
-    field: "Rollno",
-    headerName: "Roll No.",
-    width: 200,
-  },
-  {
-    field: "ViewResume",
-    headerName: "View Resume",
-    width: 200,
-  },
-  {
-    field: "AskClarification",
-    headerName: "Ask Clarification",
-    width: 200,
-    renderCell: (params) => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const [openNew, setOpenNew] = useState(false);
-      const handleOpenNew = () => {
-        setOpenNew(true);
-      };
-      const handleCloseNew = () => {
-        setOpenNew(false);
-      };
-      return (
-        <div>
-          <Modal open={openNew} onClose={handleCloseNew}>
-            <ResumeClarification
-              handleCloseNew={handleCloseNew}
-              resumeId={params.row.resumeid}
-            />
-          </Modal>
-          <ActiveButton sx={{ height: 30 }} onClick={handleOpenNew}>
-            CLICK HERE
-          </ActiveButton>
-        </div>
-      );
-    },
-  },
-  {
-    field: "Status",
-    headerName: "Status",
-    width: 300,
-    renderCell: (cellValues) => {
-      if (cellValues.row.Status === "ACCEPTED")
-        return (
-          <Stack
-            direction="row"
-            alignItems="center"
-            width="100%"
-            justifyContent="space-between"
-          >
-            <ActiveButton sx={{ height: 30 }}>
-              {cellValues.row.Status}
-            </ActiveButton>
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          </Stack>
-        );
-      // if (cellValues.row.Status === "REJECTED")
-      return (
-        <Stack
-          direction="row"
-          alignItems="center"
-          width="100%"
-          justifyContent="space-between"
-        >
-          <InactiveButton sx={{ height: 30 }}>
-            {cellValues.row.Status}
-          </InactiveButton>
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
-        </Stack>
-      );
-    },
-  },
-];
+const buttonstyle = {
+  borderRadius: 0,
+  color: "black",
+  background: "white",
+  "&:hover": { backgroundColor: "#cfd4d1" },
+};
 
 const rows = [
   {
@@ -118,6 +39,106 @@ const rows = [
 ];
 
 function Index() {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  const columns: GridColDef[] = [
+    { field: "resumeid", headerName: "Resume ID", width: 200 },
+    {
+      field: "StudentName",
+      headerName: "Student Name",
+      width: 200,
+    },
+    {
+      field: "Rollno",
+      headerName: "Roll No.",
+      width: 200,
+    },
+    {
+      field: "ViewResume",
+      headerName: "View Resume",
+      width: 200,
+    },
+    {
+      field: "AskClarification",
+      headerName: "Ask Clarification",
+      width: 200,
+      renderCell: (params) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [openNew, setOpenNew] = useState(false);
+        const handleOpenNew = () => {
+          setOpenNew(true);
+        };
+        const handleCloseNew = () => {
+          setOpenNew(false);
+        };
+        return (
+          <div>
+            <Modal open={openNew} onClose={handleCloseNew}>
+              <ResumeClarification
+                handleCloseNew={handleCloseNew}
+                resumeId={params.row.resumeid}
+              />
+            </Modal>
+            <ActiveButton sx={{ height: 30 }} onClick={handleOpenNew}>
+              CLICK HERE
+            </ActiveButton>
+          </div>
+        );
+      },
+    },
+    {
+      field: "Status",
+      headerName: "Status",
+      width: 300,
+      renderCell: (cellValues) => (
+        <Stack
+          direction="row"
+          alignItems="center"
+          width="100%"
+          justifyContent="space-between"
+        >
+          {cellValues.row.Status === "ACCEPTED" ? (
+            <ActiveButton sx={{ height: 30 }}>
+              {cellValues.row.Status}
+            </ActiveButton>
+          ) : (
+            <InactiveButton sx={{ height: 30 }}>
+              {cellValues.row.Status}
+            </InactiveButton>
+          )}
+
+          <IconButton onClick={handleClick}>
+            <MoreVertIcon />
+          </IconButton>
+          <Popover
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            <Stack direction="column">
+              <Button sx={buttonstyle}>View Profile</Button>
+              <Button sx={buttonstyle}>See History</Button>
+              <Button sx={buttonstyle}>Freeze/Unfreeze</Button>
+            </Stack>
+          </Popover>
+        </Stack>
+      ),
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <Meta title="Resume Dashboard" />
