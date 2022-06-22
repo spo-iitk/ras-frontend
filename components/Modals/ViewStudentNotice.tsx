43011372123363f96055f552bf0804e573a14481
amandeep <samandeep21@iitk.ac.in>
2022-06-22 16:05:20 +0530

    viewNotice on student site Added View Notice Modal in modal folder

---
+++ components/Modals/ViewStudentNotice.tsx
@@ -0,0 +1,52 @@
+import { Box, Stack, TextField } from "@mui/material";
+import * as React from "react";
+
+import { NoticeParams } from "@callbacks/admin/rc/notice";
+
+const boxStyle = {
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: "330px", md: "500px" },
+  bgcolor: "background.paper",
+  border: "white solid 2px",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+  alignItems: "center",
+};
+function ViewNotice({ currentNotice }: { currentNotice: NoticeParams }) {
+  let value = currentNotice.CreatedAt;
+  const publishedDateAndTime = `${new Date(
+    value
+  ).toLocaleDateString()} ${new Date(value).toLocaleTimeString()}`;
+  return (
+    <Box sx={boxStyle}>
+      <Stack spacing={3}>
+        <h1>Add Notice</h1>
+        <TextField
+          label="Company name"
+          defaultValue={currentNotice.title}
+          variant="standard"
+          disabled
+        />
+        <TextField
+          label="Published Date and Time"
+          defaultValue={publishedDateAndTime}
+          variant="standard"
+          disabled
+        />
+        <TextField
+          variant="standard"
+          disabled
+          multiline
+          label="Description"
+          defaultValue={currentNotice.description}
+        />
+      </Stack>
+    </Box>
+  );
+}
+
+export default ViewNotice;
--- pages/student/rc/[rcid]/notices.tsx
+++ pages/student/rc/[rcid]/notices.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Modal, Stack } from "@mui/material";
 import { GridColDef } from "@mui/x-data-grid";
 import React from "react";
 import { useRouter } from "next/router";
@@ -8,6 +8,7 @@ import Meta from "@components/Meta";
 import { NoticeParams } from "@callbacks/admin/rc/notice";
 import useStore from "@store/store";
 import NoticeSReq from "@callbacks/student/rc/noticeS";
+import ViewNotice from "@components/Modals/ViewStudentNotice";
 
 const columns: GridColDef[] = [
   {
@@ -37,8 +38,26 @@ function Notices() {
   const { rcid } = router.query;
   const rid = (rcid || "").toString();
   const { token } = useStore();
+  const [openNew, setOpenNew] = React.useState(false);
+  const handleOpenNew = () => {
+    setOpenNew(true);
+  };
+  const handleCloseNew = () => {
+    setOpenNew(false);
+  };
   const [notices, setNotice] = React.useState<NoticeParams[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
+  const [currentNotice, setCurrentNotice] = React.useState<NoticeParams>({
+    ID: 0,
+    recruitment_cycle_id: 0,
+    title: "",
+    description: "",
+    tags: "",
+    attachment: "",
+    created_by: "",
+    CreatedAt: "",
+    last_reminder_at: 0,
+  });
   React.useEffect(() => {
     const fetch = async () => {
       if (rid === undefined || rid === "") return;
@@ -67,8 +86,15 @@ function Notices() {
           getRowId={(row) => row.ID}
           columns={columns}
           loading={loading}
+          onCellClick={(params) => {
+            setCurrentNotice(params.row);
+            handleOpenNew();
+          }}
         />
       </Stack>
+      <Modal open={openNew} onClose={handleCloseNew}>
+        <ViewNotice currentNotice={currentNotice} />
+      </Modal>
     </div>
   );
 }
