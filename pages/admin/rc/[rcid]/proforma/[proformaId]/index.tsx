e6f41be834e8fce3aa5023ade9779508d4519e14
bangaradi <adityavb@cse.iitk.ac.in>
2022-06-23 20:28:13 +0530

    add company show/hide details

--- pages/admin/rc/[rcid]/proforma/[proformaId]/index.tsx
+++ pages/admin/rc/[rcid]/proforma/[proformaId]/index.tsx
@@ -1,9 +1,37 @@
-import { Button, Card, Grid, Stack } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  Grid,
+  Modal,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { GridColDef } from "@mui/x-data-grid";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 
 import DataGrid from "@components/DataGrid";
 import Meta from "@components/Meta";
+import requestProforma, {
+  AdminProformaType,
+} from "@callbacks/admin/rc/adminProforma";
+import useStore from "@store/store";
+
+const boxStyle = {
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: { xs: "330px", md: "500px" },
+  bgcolor: "background.paper",
+  border: "white solid 2px",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+  alignItems: "center",
+};
 
 const columns: GridColDef[] = [
   {
@@ -43,6 +71,44 @@ const rows = [
   },
 ];
 function Index() {
+  const {
+    register,
+    // handleSubmit,
+    // formState: { errors },
+    // reset,
+  } = useForm<AdminProformaType>();
+
+  const [proformaData, setProformaData] = useState<AdminProformaType>();
+  const [openEmailSender, setOpenEmailSender] = useState(false);
+  const handleOpenEmailSender = () => {
+    setOpenEmailSender(true);
+  };
+  const handleCloseEmailSender = () => {
+    setOpenEmailSender(false);
+  };
+  const [openDateChanger, setOpenDateChanger] = useState(false);
+  const handleOpenDateChanger = () => {
+    setOpenDateChanger(true);
+  };
+  const handleCloseDateChanger = () => {
+    setOpenDateChanger(false);
+  };
+  const router = useRouter();
+  const { rcid } = router.query;
+  const { proformaId } = router.query;
+  const { token } = useStore();
+
+  const handleProformaData = (data: boolean) => {
+    setProformaData({ ...proformaData, hide_details: data });
+  };
+  useEffect(() => {
+    const fetchAdminProforma = async () => {
+      const response = await requestProforma.get(token, rcid, proformaId);
+      setProformaData(response);
+    };
+
+    fetchAdminProforma();
+  }, [token, rcid, proformaId]);
   return (
     <div className="container">
       <Meta title="Proforma" />
@@ -54,10 +120,22 @@ function Index() {
         direction={{ lg: "row", xs: "column" }}
       >
         <Stack spacing={3} direction={{ sm: "row", xs: "column" }}>
-          <Button sx={{ width: { xs: "280px" } }} variant="contained">
+          <Button
+            sx={{ width: { xs: "280px" } }}
+            variant="contained"
+            onClick={() => {
+              router.push(`/admin/rc/${rcid}/proforma/${proformaId}/view`);
+            }}
+          >
             View IP
           </Button>
-          <Button sx={{ width: { xs: "280px" } }} variant="contained">
+          <Button
+            sx={{ width: { xs: "280px" } }}
+            variant="contained"
+            onClick={() => {
+              router.push(`/admin/rc/${rcid}/proforma/${proformaId}/step1`);
+            }}
+          >
             Update IP
           </Button>
         </Stack>
@@ -66,7 +144,13 @@ function Index() {
             Accept IP
           </Button>
 
-          <Button sx={{ width: { xs: "280px" } }} variant="contained">
+          <Button
+            sx={{ width: { xs: "280px" } }}
+            variant="contained"
+            onClick={() => {
+              router.push(`/admin/rc/${rcid}/proforma/${proformaId}/question`);
+            }}
+          >
             View / Add Custom Questions
           </Button>
         </Stack>
@@ -97,13 +181,22 @@ function Index() {
                 <Button
                   variant="contained"
                   sx={{ width: { xs: "280px", md: "250px" }, height: "60px" }}
+                  onClick={handleProformaData(false)}
                 >
-                  Show /Hide Details To Company
+                  Show Details of Company
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{ width: { xs: "280px", md: "250px" }, height: "60px" }}
+                  onClick={handleProformaData(true)}
+                >
+                  Hide Details of Company
                 </Button>
 
                 <Button
                   variant="contained"
                   sx={{ width: { xs: "280px", md: "250px" }, height: "60px" }}
+                  onClick={handleOpenDateChanger}
                 >
                   Change/Set Deadline
                 </Button>
@@ -112,16 +205,39 @@ function Index() {
                 <Button
                   variant="contained"
                   sx={{ width: { xs: "280px", md: "250px" }, height: "60px" }}
-                >
-                  Send Reminder
-                </Button>
-
-                <Button
-                  variant="contained"
-                  sx={{ width: { xs: "280px", md: "250px" }, height: "60px" }}
+                  onClick={handleOpenEmailSender}
                 >
                   Send Customised Email
                 </Button>
+                <Modal open={openEmailSender} onClose={handleCloseEmailSender}>
+                  <Box sx={boxStyle}>
+                    <Stack spacing={3}>
+                      <h1>Enter Email to be Sent</h1>
+                      <TextField
+                        label="Enter New Password"
+                        variant="standard"
+                      />
+                      <Button variant="contained" sx={{ width: "100%" }}>
+                        Submit
+                      </Button>
+                    </Stack>
+                  </Box>
+                </Modal>
+                <Modal open={openDateChanger} onClose={handleCloseDateChanger}>
+                  <Box sx={boxStyle}>
+                    <Stack spacing={3}>
+                      <h1>Enter New Deadline</h1>
+                      <TextField
+                        type="date"
+                        variant="standard"
+                        {...register("set_deadline")}
+                      />
+                      <Button variant="contained" sx={{ width: "100%" }}>
+                        Submit
+                      </Button>
+                    </Stack>
+                  </Box>
+                </Modal>
               </Stack>
             </Stack>
           </Grid>
