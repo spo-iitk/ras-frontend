5fbe9aaa762d3b72e16710964fa5a75422d7f4f8
utkarsh-tf141 <utkarshm21@iitk.ac.in>
2022-06-23 20:16:53 +0530

    Student proforma & events added

--- pages/student/rc/[rcid]/proforma/[proformaId]/index.tsx
+++ pages/student/rc/[rcid]/proforma/[proformaId]/index.tsx
@@ -1,9 +1,14 @@
 import { Card, Grid, Stack, TextField } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 import Meta from "@components/Meta";
 import StepperComp from "@components/Stepper/stepperComp";
 import MatrixExpanded from "@components/Utils/MatrixExpanded";
+import useStore from "@store/store";
+import { ProformaEvent, ProformaType } from "@callbacks/company/proforma";
+import sProformaRequest from "@callbacks/student/rc/proforma";
+import RichText from "@components/Editor/RichText";
 
 const textFieldColor = "#ff0000";
 const textFieldSX = {
@@ -14,44 +19,39 @@ const textFieldSX = {
   },
 };
 
-const info = [
-  {
-    label: "Company Name",
-    value: "Paradime",
-  },
-  {
-    label: "Nature of Business",
-    value: "FrontEnd Engineer",
-  },
-  {
-    label: "Tentative Job Location",
-    value: "London",
-  },
-  {
-    label: "Job Description",
-    value: "Product Engineer in FrontEnd",
-  },
-  {
-    label: "Cost to Company",
-    value: "Not to be told",
-  },
-  {
-    label: "Package Details",
-    value: "Enough Money",
-  },
-  {
-    label: "Bond Details",
-    value: "Yes",
-  },
-  {
-    label: "Medical Requirements",
-    value: "None",
-  },
-];
-
-const data = new Array(100 + 1).join("0");
+const data1 = new Array(100 + 1).join("0");
 
 function Index() {
+  const { token } = useStore();
+  const router = useRouter();
+  const { rcid } = router.query;
+  const PID = router.query.proformaId;
+  const rid = (rcid || "").toString();
+  const ID = (PID || "").toString();
+  const [ctc, setCtc] = useState("");
+  const [jd, setJd] = useState("");
+  const [pd, setPd] = useState("");
+  const [isFetched, setisFetched] = useState(false);
+  const [row, setRow] = useState<ProformaType>({
+    ID: 0,
+  } as ProformaType);
+  const [row2, setRow2] = useState<ProformaEvent[]>([]);
+  useEffect(() => {
+    const getCompanydata = async () => {
+      if (rid === undefined || rid === "") return;
+      if (ID === undefined || ID === "") return;
+      let response = await sProformaRequest.get(token, rid, ID);
+      setRow(response);
+      let response2 = await sProformaRequest.getEvent(token, rid, ID);
+      setRow2(response2);
+      setCtc(response.cost_to_company);
+      setJd(response.job_description);
+      setPd(response.package_details);
+      setisFetched(true);
+    };
+    getCompanydata();
+  }, [token, rid, ID]);
+  const data = row.eligibility?.length > 90 ? row.eligibility : data1;
   return (
     <div style={{ padding: "0 2rem", marginBottom: 20 }}>
       <Meta title="Company - Proforma" />
@@ -66,31 +66,90 @@ function Index() {
       >
         <Stack spacing={2}>
           <Grid container spacing={2}>
-            {info.map((item) => (
-              <Grid
-                item
-                xs={12}
-                md={item.label === "Eligibility" ? 12 : 6}
-                key={item.label}
-              >
-                <h3>{item.label}</h3>
-                <TextField
-                  multiline
-                  fullWidth
-                  value={item.value}
-                  variant="standard"
-                  disabled
-                  sx={textFieldSX}
-                />
-              </Grid>
-            ))}
+            <Grid item xs={12} md={12} key="bnature">
+              <h3>Company Name</h3>
+              <TextField
+                multiline
+                fullWidth
+                value={row.company_name}
+                InputProps={{
+                  style: { textAlign: "center" },
+                  readOnly: true,
+                }}
+                sx={textFieldSX}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} key="bnature">
+              <h3>Nature of Business</h3>
+              <TextField
+                multiline
+                fullWidth
+                minRows={4}
+                value={row.nature_of_business}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={textFieldSX}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} key="tjobloc">
+              <h3>Tentative Job Location</h3>
+              <TextField
+                multiline
+                fullWidth
+                minRows={4}
+                value={row.tentative_job_location}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={textFieldSX}
+              />
+            </Grid>
+            <Grid item xs={12} md={12} key="jd">
+              <h3>Job Description</h3>
+              {isFetched && <RichText onChange={setJd} readOnly value={jd} />}
+            </Grid>
+            <Grid item xs={12} md={12} key="ctc">
+              <h3>Cost to Company</h3>
+              {isFetched && <RichText onChange={setCtc} readOnly value={ctc} />}
+            </Grid>
+            <Grid item xs={12} md={12} key="pd">
+              <h3>Package Details</h3>
+              {isFetched && <RichText onChange={setPd} readOnly value={pd} />}
+            </Grid>
+            <Grid item xs={12} md={6} key="bond">
+              <h3>Bond Details</h3>
+              <TextField
+                multiline
+                fullWidth
+                minRows={4}
+                value={row.bond_details}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={textFieldSX}
+              />
+            </Grid>
+            <Grid item xs={12} md={6} key="MedReq">
+              <h3>Medical Requirements</h3>
+              <TextField
+                multiline
+                fullWidth
+                minRows={4}
+                value={row.medical_requirements}
+                InputProps={{
+                  readOnly: true,
+                }}
+                sx={textFieldSX}
+              />
+            </Grid>
             <Grid item xs={12}>
               <h3>Eligibility</h3>
               <MatrixExpanded data={data} />
             </Grid>
             <Grid item xs={12}>
               <h3>Hiring Process</h3>
-              <StepperComp />
+              <StepperComp steps={row2} />
             </Grid>
           </Grid>
         </Stack>
