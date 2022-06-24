62ed27473b1cbde347d6f4731347cf394aac27fc
Tejas Ahuja <ahujatejas06@gmail.com>
2022-06-24 20:45:42 +0530

    overview page

---
+++ callbacks/admin/rc/overview.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+import {
+  ADMIN_APPLICATION_URL,
+  ErrorType,
+  SERVER_ERROR,
+  responseBody,
+  setConfig,
+} from "@callbacks/constants";
+import { errorNotification } from "@callbacks/notifcation";
+
+export interface Event {
+  duration: number;
+  name: string;
+  ID: number;
+  proforma_id: number;
+  sequence: number;
+  start_date: number;
+  start_time: number;
+  end_time: number;
+  main_poc: string;
+  description: string;
+  record_attendance: boolean;
+  venue: string;
+}
+const instance = axios.create({
+  baseURL: ADMIN_APPLICATION_URL,
+  timeout: 15000,
+  timeoutErrorMessage: SERVER_ERROR,
+});
+
+const eventRequest = {
+  getAll: (token: string, rcid: string) =>
+    instance
+      .get<Event[]>(`/rc/${rcid}/event`, setConfig(token))
+      .then(responseBody)
+      .catch((err: ErrorType) => {
+        errorNotification("Error", err.response?.data?.error || err.message);
+        return [] as Event[];
+      }),
+};
+export default eventRequest;
