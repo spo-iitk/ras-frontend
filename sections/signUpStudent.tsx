e51734db0f23b6fc773ec3e2e5042d22faeb3abc
Harshit Raj <75195728+1-Harshit@users.noreply.github.com>
2022-07-19 01:34:36 +0530

    Update sections/signUpStudent.tsx

--- sections/signUpStudent.tsx
+++ sections/signUpStudent.tsx
@@ -50,7 +50,7 @@ function SignUpStudent() {
     str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
   const titleCase = (str: string) => {
-    const splitStr = str.toLowerCase().split(" ").map(capitalizeFirstLetter);
+    const splitStr = str.trim().toLowerCase().split(" ").map(capitalizeFirstLetter);
     return splitStr.join(" ");
   };
 
