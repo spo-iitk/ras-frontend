2c13a2199803d013012115661bd98ea1348df9df
Manas Gupta <mmanasggupta@gmail.com>
2022-07-18 12:17:20 +0530

    Remove static  headers and add exam mapper (#174)
    * Fix stats
    
    * Add admin calendar events
    
    * integrate calendar in student side
    
    * Fix exam mapping

--- callbacks/student/rc/events.ts
+++ callbacks/student/rc/events.ts
@@ -7,10 +7,8 @@ import {
   setConfig,
 } from "@callbacks/constants";
 import { errorNotification } from "@callbacks/notifcation";
+import { Event } from "@callbacks/admin/rc/proforma/event";
 
-export interface events {
-  id: number;
-}
 const instance = axios.create({
   baseURL: STUDENT_URL,
   timeout: 15000,
@@ -20,13 +18,21 @@ const instance = axios.create({
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const eventsRequest = {
-  getAll: (token: string) =>
+  getAll: (token: string, rcid: string) =>
     instance
-      .get("/events", setConfig(token))
+      .get(`/application/rc/${rcid}/event`, setConfig(token))
       .then(responseBody)
       .catch((err: ErrorType) => {
         errorNotification("Error", err.response?.data?.error || err.message);
-        return [] as events[];
+        return [] as Event[];
+      }),
+  get: (token: string, rcid: string, eventid: string) =>
+    instance
+      .get(`/application/rc/${rcid}/event/${eventid}`, setConfig(token))
+      .then(responseBody)
+      .catch((err: ErrorType) => {
+        errorNotification("Error", err.response?.data?.error || err.message);
+        return {} as Event;
       }),
 };
 export default eventsRequest;
