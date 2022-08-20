20131ac778b99eca27c26a2f21aabfab8b68d0e8
TEJAS AHUJA <35026251+ahujatejas06@users.noreply.github.com>
2022-08-20 14:49:58 +0530

    Hide the event sequence on events page by default (#180)
    * Hid the event sequence on events page by default
    
    * inserted ,
    
    * Made changes

--- pages/admin/rc/[rcid]/event/index.tsx
+++ pages/admin/rc/[rcid]/event/index.tsx
@@ -50,10 +50,6 @@ const columns: GridColDef[] = [
     renderCell: (params) => new Date(params.row.start_time).toLocaleString(),
   },
   {
-    field: "sequence",
-    headerName: "Sequence",
-  },
-  {
     field: "View Details",
     renderCell: (params) => (
       <Link
