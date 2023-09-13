085bb8e5883a94664bbae24c58daf92dd44e00b4
utkarsh-tf141 <utkarshm21@iitk.ac.in>
2023-09-13 22:24:27 +0530

    added RC 6 Calendar

--- components/Utils/calendarUtils.ts
+++ components/Utils/calendarUtils.ts
@@ -11,6 +11,10 @@ export const calendarLinks = new Map([
     "5",
     "https://calendar.google.com/calendar/u/1?cid=OWU1ZDdkY2NkYTUwODMwM2E4MjYyYjE4ODAyNWRmMWUwZjQ2YzIwOTE4ZjYwYTM5YWJiZmQ0MzU0ZDZkZTg5M0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t",
   ],
+  [
+    "6",
+    "https://calendar.google.com/calendar/u/1?cid=ZjhmNThhMzVmNTZmYTUzMDVkOTUxNTcxYzg5ZTVhYzQwZDI0NDk1NmVkNWVlOWY1NGFhZjU3ZWMyODFjZjY0ZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t",
+  ],
 ]);
 
 export default calendarLinks;
