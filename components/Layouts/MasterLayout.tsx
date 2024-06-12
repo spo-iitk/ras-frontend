af19855d3eb3ade6ece9ce20d695c898efa9ccfe
sandeep14k <hiiamsandeepjha@gmail.com>
2024-06-12 18:55:27 +0530

    cache img

--- components/Layouts/MasterLayout.tsx
+++ components/Layouts/MasterLayout.tsx
@@ -236,6 +236,20 @@ function MasterLayout({
     </Box>
   );
 
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+          .then((registration) => {
+            console.log('Service Worker registered with scope: ', registration.scope);
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed:', error);
+          });
+      });
+    }
+  }, []);
+
   return (
     <div>
       <Grid
---
+++ public/Service-worker.js
@@ -0,0 +1,29 @@
+// public/service-worker.js
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+    })
+  );
+});``
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.url.startsWith('https://oa.cc.iitk.ac.in/Oa/Jsp/Photo/')) {
+    event.respondWith(
+      caches.match(request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(request).then((networkResponse) => {
+          return caches.open('v1').then((cache) => {
+            cache.put(request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+  }
+});
