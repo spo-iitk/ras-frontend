52c002c72a9b61c485cd6b295b19fd39518e0fc9
AkshatIITk <iitkakshat@gmail.com>
2024-06-18 00:43:01 +0530

    added required in desired fields + updated edit performa step 1 (company side) made similar to new.tsx + minor change in step 2 (company side)

--- pages/company/rc/[rcid]/proforma/[proformaid]/step1.tsx
+++ pages/company/rc/[rcid]/proforma/[proformaid]/step1.tsx
@@ -1,16 +1,32 @@
-import { Button, Card, FormControl, Stack, TextField } from "@mui/material";
+import {
+  Button,
+  Card,
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  TextField,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 
 import Meta from "@components/Meta";
-import RichTextEditor from "@components/Editor/RichText";
+// import RichTextEditor from "@components/Editor/RichText";
 import proformaRequest, { ProformaType } from "@callbacks/company/proforma";
 import useStore from "@store/store";
 
 const ROUTE = "/company/rc/[rcId]/proforma/[proformaid]/step2";
 function ProformaNew() {
-  const [value, onChange] = useState("");
+  const [industrySectors, setIndustrySectors] = useState<string[]>([]);
+  const [type, setType] = useState<string>("");
+  const [industrySectorsString, setIndustrySectorsString] =
+    useState<string>("");
+  const [jobDesc, setJobDesc] = useState("");
   const { token, name } = useStore();
   const router = useRouter();
   const { rcid, proformaid } = router.query;
@@ -32,18 +48,37 @@ function ProformaNew() {
     const getStep1 = async () => {
       const data = await proformaRequest.get(token, rid, pid);
       setFetch(data);
-      onChange(data.job_description);
+      setIndustrySectors(data.role.split(","));
+      setJobDesc(data.job_description);
+      setType(data.type_of_org);
       reset(data);
     };
     getStep1();
   }, [rid, pid, token, reset]);
+  const handleIndustrySectorChange = (event: SelectChangeEvent<string[]>) => {
+    const {
+      target: { value },
+    } = event;
+    const selectedValues = typeof value === "string" ? value.split(",") : value;
+    setIndustrySectors(selectedValues);
+    setIndustrySectorsString(selectedValues.join(", ")); // Store the comma-separated string
+  };
+  const handleTypeChange = (event: SelectChangeEvent<string>) => {
+    const {
+      target: { value },
+    } = event;
+    const selectedValue = value;
+    setType(selectedValue);
+  };
 
   const handleNext = async (data: ProformaType) => {
     const info: ProformaType = {
       ...data,
       ID: parseInt(pid, 10),
-      job_description: value,
+      job_description: jobDesc,
       recruitment_cycle_id: parseInt(rid, 10),
+      role: industrySectorsString,
+      type_of_org: type,
     };
     await proformaRequest.put(token, rid, info).then(() => {
       reset({
@@ -51,7 +86,7 @@ function ProformaNew() {
         role: "",
         tentative_job_location: "",
       });
-      onChange("");
+      setJobDesc("");
       router.push({
         pathname: ROUTE,
         query: { rcId: rid, proformaid: pid },
@@ -61,7 +96,7 @@ function ProformaNew() {
 
   return (
     <div>
-      <Meta title="Step 1/6 - Basic Details" />
+      <Meta title="New Proforma" />
       <Card
         elevation={5}
         sx={{
@@ -70,7 +105,7 @@ function ProformaNew() {
         }}
       >
         <Stack spacing={3}>
-          <h2>Step 1 : Basic Details</h2>
+          <h2>Step 1/6 : Basic Details</h2>
           <FormControl sx={{ m: 1 }}>
             <p style={{ fontWeight: 300 }}>Company Name</p>
             <TextField
@@ -81,75 +116,191 @@ function ProformaNew() {
               fullWidth
               multiline
               variant="standard"
-              {...register("company_name")}
+              error={!!errors.company_name}
+              helperText={errors.company_name?.message}
+              {...register("company_name", { required: true })}
             />
           </FormControl>
           <FormControl sx={{ m: 1 }}>
-            <p style={{ fontWeight: 300 }}>Nature of Business</p>
+            <p style={{ fontWeight: 300 }}>Postal Address</p>
             <TextField
-              id="NatureOfBuisness"
+              id="postalAddress"
               required
-              sx={{ marginLeft: "5 rem" }}
               fullWidth
               multiline
               variant="standard"
-              error={!!errors.role}
-              helperText={errors.role?.message}
-              {...register("role", {
-                required: "Role is required",
-                maxLength: {
-                  value: 100,
-                  message: "Role length should be less than 100",
-                },
+              error={!!errors.postal_address}
+              helperText={errors.postal_address?.message}
+              {...register("postal_address", {
+                required: "Postal Address is required",
               })}
             />
           </FormControl>
+
           <FormControl sx={{ m: 1 }}>
-            <p style={{ fontWeight: 300 }}>Profile</p>
+            <p style={{ fontWeight: 300 }}>Date of Establishment</p>
             <TextField
-              id="Profile"
+              id="dateOfEstablishment"
+              type="date"
               required
-              sx={{ marginLeft: "5 rem" }}
               fullWidth
-              multiline
               variant="standard"
-              error={!!errors.profile}
-              helperText={errors.profile?.message}
-              {...register("profile", {
-                required: "Profile is required",
-                maxLength: {
-                  value: 100,
-                  message: "Profile length should be less than 100",
-                },
+              error={!!errors.establishment_date}
+              helperText={errors.establishment_date?.message}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              {...register("establishment_date", {
+                required: "Date of Establishment is required",
               })}
             />
           </FormControl>
+
           <FormControl sx={{ m: 1 }}>
-            <p style={{ fontWeight: 300 }}>Tentative Job Location</p>
+            <p style={{ fontWeight: 300 }}>Number of Employees</p>
             <TextField
-              id="TentativeJobLocation"
+              id="numberOfEmployees"
               required
-              sx={{ marginLeft: "5 rem" }}
               fullWidth
-              multiline
               variant="standard"
-              error={!!errors.tentative_job_location}
-              helperText={
-                errors.tentative_job_location && "This field is required"
-              }
-              {...register("tentative_job_location", { required: true })}
+              error={!!errors.total_employees}
+              helperText={errors.total_employees?.message}
+              {...register("total_employees", {
+                required: "Number of Employees is required",
+              })}
             />
           </FormControl>
-          {fetchData.ID !== 0 && (
-            <FormControl sx={{ m: 1 }}>
-              <p style={{ fontWeight: 300 }}>Job Description</p>
-              <RichTextEditor
-                value={fetchData.job_description}
-                onChange={onChange}
-                style={{ minHeight: 200 }}
-              />
-            </FormControl>
-          )}
+
+          <FormControl sx={{ m: 1 }}>
+            <p style={{ fontWeight: 300 }}>Social Media Page Link (optional)</p>
+            <TextField
+              id="socialMediaPageLink"
+              fullWidth
+              variant="standard"
+              {...register("social_media")}
+            />
+          </FormControl>
+
+          <FormControl sx={{ m: 1 }}>
+            <p style={{ fontWeight: 300 }}>Website</p>
+            <TextField
+              id="website"
+              required
+              fullWidth
+              variant="standard"
+              error={!!errors.website}
+              helperText={errors.website?.message}
+              {...register("website", {
+                required: "Website is required",
+              })}
+            />
+          </FormControl>
+
+          <FormControl sx={{ m: 1 }}>
+            <p style={{ fontWeight: 300 }}>Company Turnover for NIRF Purpose</p>
+            <TextField
+              id="companyTurnover"
+              required
+              fullWidth
+              variant="standard"
+              error={!!errors.turnover}
+              helperText={errors.turnover?.message}
+              {...register("turnover", {
+                required: "Company Turnover is required",
+              })}
+            />
+          </FormControl>
+
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel>Type of Organization</InputLabel>
+            <Select
+              id="typeOfOrganization"
+              required
+              fullWidth
+              variant="standard"
+              value={type}
+              onChange={handleTypeChange}
+              renderValue={(selected) => selected}
+              // {...register("type_of_org", {
+              //   required: "Type of Organization is required",
+              // })}
+            >
+              <MenuItem value="Private (Indian/Foreign)">
+                Private (Indian/Foreign)
+              </MenuItem>
+              <MenuItem value="Multi National Company (Indian Origin)">
+                Multi National Company (Indian Origin)
+              </MenuItem>
+              <MenuItem value="Multi National Company (Foreign Origin)">
+                Multi National Company (Foreign Origin)
+              </MenuItem>
+              <MenuItem value="Government">Government</MenuItem>
+              <MenuItem value="Public Sector Undertakings (Indian)">
+                Public Sector Undertakings (Indian)
+              </MenuItem>
+              <MenuItem value="Non-Government Organisation">
+                Non-Government Organisation
+              </MenuItem>
+              <MenuItem value="STARTUP">STARTUP</MenuItem>
+              <MenuItem value="Others">Others</MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ m: 1 }}>
+            <p style={{ fontWeight: 300 }}>
+              Location and Head office of the parent company (if MnC)
+            </p>
+            <TextField
+              id="mncDetails"
+              fullWidth
+              variant="standard"
+              error={!!errors.head_office}
+              helperText={errors.head_office?.message}
+              {...register("head_office", {
+                required: "Head Office is required",
+              })}
+            />
+          </FormControl>
+
+          <FormControl sx={{ m: 1 }}>
+            <InputLabel>Nature of Business / Industry Sector</InputLabel>
+            <Select
+              id="NatureOfBusiness"
+              multiple
+              fullWidth
+              required
+              variant="standard"
+              value={industrySectors}
+              onChange={handleIndustrySectorChange}
+              renderValue={(selected) => selected.join(", ")}
+            >
+              {[
+                "Core Engineering & Technology",
+                "Analytics",
+                "IT / Software",
+                "Oil & Gas / Energy",
+                "Data Science/ AI/ ML",
+                "Cyber Security",
+                "Finance & Consulting",
+                "Management",
+                "Academics/Research",
+                "Media",
+                "E-Commerce",
+                "Construction",
+                "Design",
+                "Manufacturing",
+                "Infrastructure",
+                "HealthCare/ Biomedical",
+                "Edutech",
+                "Policy",
+              ].map((sector) => (
+                <MenuItem key={sector} value={sector}>
+                  <Checkbox checked={industrySectors.indexOf(sector) > -1} />
+                  <ListItemText primary={sector} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
 
           <Stack
             spacing={3}
@@ -172,9 +323,7 @@ function ProformaNew() {
                 reset({
                   company_name: "",
                   role: "",
-                  tentative_job_location: "",
                 });
-                onChange("");
               }}
             >
               Reset
