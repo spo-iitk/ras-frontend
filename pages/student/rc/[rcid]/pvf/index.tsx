8abb90a0dd117035ff4dd4b1ec3d7da254f9824c
VineeshD06 <vineeshdubey06@gmail.com>
2025-06-06 22:35:58 +0530

    fix: resolve Prettier formatting errors

--- pages/student/rc/[rcid]/pvf/index.tsx
+++ pages/student/rc/[rcid]/pvf/index.tsx
@@ -531,9 +531,10 @@ function PVF() {
                 {...register("mentor_email", {
                   required: "This field is required",
                   pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@(?!(gmail|yahoo|hotmail|outlook|mail|rediffmail|protonmail|icloud|zoho|yandex)\.)[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: "Please enter a valid company or university email",
-                },
+                    value:
+                      /^[a-zA-Z0-9._%+-]+@(?!(gmail|yahoo|hotmail|outlook|mail|rediffmail|protonmail|icloud|zoho|yandex)\.)[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Please enter a valid company or university email",
+                  },
                 })}
               />
             </FormControl>
